c8a957b036e21750cac9d8d3978df694b1c29927
Daniel Yangua <dannyyanguacorrea@hotmail.com>
2019-12-20 09:54:17 -0500

    add aribnb eslint

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
-import React from 'react'
-import Head from 'next/head'
+import React from 'react';
+import Head from 'next/head';
 
 const Home = () => (
   <div>
     <Head>
       <title>Home</title>
       <link rel="icon" href="/favicon.ico" />
-      <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
+      <link
+        rel="stylesheet"
+        href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
+      />
     </Head>
     <h1>Welcome to Next js</h1>
   </div>
-)
+);
 
-export default Home
+export default Home;
